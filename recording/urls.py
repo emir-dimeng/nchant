33cc77386d6d93ae5818b91ddd48d559ed18af50
di <di.meng@ucdconnect.ie>
2020-07-09 11:05:20 +0100

    upload all code to git hub

---
+++ recording/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+from . import views
+from django.conf.urls import url
+
+app_name = 'recording'
+urlpatterns = [
+	# ex: /record/
+	path('', views.IndexView.as_view(), name='index'),
+	path('test/', views.test, name='test'),
+
+	# ex: /record/5/
+	path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+	path('new_seed', views.new_seed, name='new_seed'),
+
+	url(r'^ajax/align_voice/$', views.align_voice, name='align_voice'),
+
+	url(r'^ajax/create_seed/$', views.create_seed, name='create_seed'),
+	# ex: /record/5/results/
+	#path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+	# ex: /record/5/vote/
+	#path('<int:question_id>/vote/', views.vote, name='vote'),
+]
